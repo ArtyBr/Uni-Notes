4525ff65ae1e2819007f925a12ae129e2b8d1785
Artys Lenovo Ubuntu <artemiy.brukhno@gmail.com>
2026-05-11 15:53:47 +0100

    Update baseUrl to correct GitHub Pages path

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "uni-notes.github.io",
+    baseUrl: "ArtyBr.github.io/Uni-Notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
