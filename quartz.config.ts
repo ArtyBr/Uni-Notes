e7b89fa3153593e8a28c7d7d2ad856c11ec69727
Artys Lenovo Ubuntu <artemiy.brukhno@gmail.com>
2026-05-12 10:42:05 +0100

    feat: update site title and disable reading time

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "University Notes",
+    pageTitle: "Warwick CS Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
